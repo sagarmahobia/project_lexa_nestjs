426dd2385a42ce7d11dabe1ed406775cf5c89802
ssoni <suraj.soni@infor.com>
2023-12-23 23:32:17 +0530

    myfirtchange

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(){
+    var s = this.appService.getHello();
+    return { 
+      "success":true,
+      "data":s,
+      "message":"you got message"
+    };
   }
 }
